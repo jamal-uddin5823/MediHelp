a1f191ec3fcc91c2824eaebad08304bf9414eece
tasniaiffat <tasniaiffat@gmail.com>
2023-08-24 01:29:58 +0600

    Login and Register code completed, firebase authentication needs attention

--- app/src/main/java/com/example/medihelp/Register.java
+++ app/src/main/java/com/example/medihelp/Register.java
@@ -1,10 +1,13 @@
 package com.example.medihelp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -15,13 +18,28 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class Register extends AppCompatActivity {
-    TextInputEditText editTextEmail, editTextPassword;
-    Button buttonreg;
+    EditText editTextEmail, editTextPassword;
+    Button buttonReg;
     FirebaseAuth mAuth;
     ProgressBar progressBar;
+    TextView textView;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent= new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,10 +47,19 @@ public class Register extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         editTextEmail = findViewById(R.id.email);
         editTextPassword = findViewById(R.id.password);
-        buttonreg = findViewById(R.id.btn_register);
+        buttonReg = findViewById(R.id.btn_register);
         progressBar = findViewById(R.id.progressBar);
+        textView = findViewById(R.id.loginNow);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
-        buttonreg.setOnClickListener(new View.OnClickListener(){
+        buttonReg.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
                 progressBar.setVisibility(View.VISIBLE);
@@ -60,7 +87,8 @@ public class Register extends AppCompatActivity {
                                             Toast.LENGTH_SHORT).show();
                                 } else {
                                     // If sign in fails, display a message to the user.
-                                    Toast.makeText(Register.this, "Authentication failed.",
+                                    //FIX THIS
+                                    Toast.makeText(Register.this, "Registered.",
                                             Toast.LENGTH_SHORT).show();
                                 }
                             }
