b0f85acdfb7b22fbe85d114b5a0f15dbd6643a8c
tasniaiffat <tasniaiffat@gmail.com>
2023-08-24 06:58:38 +0600

    green theme created, but implementing lavendar theme atm

--- app/src/main/java/com/example/medihelp/Register.java
+++ app/src/main/java/com/example/medihelp/Register.java
@@ -85,11 +85,17 @@ public class Register extends AppCompatActivity {
                                 if (task.isSuccessful()) {
                                     Toast.makeText(Register.this, "Account Created",
                                             Toast.LENGTH_SHORT).show();
+                                    Intent intent= new Intent(getApplicationContext(), MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
                                 } else {
                                     // If sign in fails, display a message to the user.
                                     //FIX THIS
                                     Toast.makeText(Register.this, "Registered.",
                                             Toast.LENGTH_SHORT).show();
+                                    Intent intent= new Intent(getApplicationContext(), MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
                                 }
                             }
                         });
