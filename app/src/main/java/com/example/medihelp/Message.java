52c45c72ea5e3f926565a6bdd91b0636234c085e
Jamal <jamaluddin10775@gmail.com>
2023-11-02 01:34:59 +0600

    Non medical queries blocked

--- app/src/main/java/com/example/medihelp/Message.java
+++ app/src/main/java/com/example/medihelp/Message.java
@@ -16,12 +16,17 @@ public String getContent() {
 }
 
 public void setContent(String messages) {
-        String age = MainActivity.currentUserData.getAge();
-        if(age==null)age="18";
-        String gender =MainActivity.currentUserData.getGender();
-        if(gender==null)gender="Female";
+        String age="25";
+        String gender = "any";
+        if(MainActivity.currentUserData.getAge()!=null) {
+            age = MainActivity.currentUserData.getAge();
+            if(age==null)age="18";
+            gender =MainActivity.currentUserData.getGender();
+            if(gender==null)gender="Female";
+        }
 
-        this.content = "You are an AI assistant that is an expert in medical health. You know about symptoms and signs of various types of illnesses. Recommend the medical history queries with which specialist they should consult.  Format any lists on individual lines with a dash and a space in front of each line.I am a " + age + " year old " + gender + ".My symptoms are " + messages + ".Just give one word answer.";
+
+        this.content = "You are an AI assistant that is an expert in medical health. You know about symptoms and signs of various types of illnesses. Recommend the medical history queries with which specialist they should consult.  Format any lists on individual lines with a dash and a space in front of each line. If it is not a symptom or illness, reply Sorry, only medical queries allowed. I am a " + age + " year old " + gender + ".My symptoms are " + messages + ".Just give one word answer.";
     }
 
 }
